8005185fe42fe5826e435028f9e3c697a6a9c972
ahb <andreas.bihlmaier@gmx.de>
2014-02-17 17:15:12 +0100

    must obey pose ... see TODOs

--- scripts/gazebo2marker_node.py
+++ scripts/gazebo2marker_node.py
@@ -4,9 +4,9 @@ import os
 import xml.etree.ElementTree as ET
 
 import rospy
-#import tf
-#from geometry_msgs.msg import Pose
-#import tf_conversions.posemath as pm
+import tf
+from geometry_msgs.msg import Pose
+import tf_conversions.posemath as pm
 from gazebo_msgs.msg import ModelStates
 from visualization_msgs.msg import Marker
 
@@ -16,7 +16,7 @@ from gazebo2rviz.model_names import *
 lastUpdateTime = None
 updatePeriod = 5
 markerPub = None
-modelDict = {} # modelName -> [(tfName1, meshPath1), ...]
+modelDict = {} # modelName -> [(tfName1, meshPose1, meshPath1), ...]
 modelsPath = os.path.expanduser('~/.gazebo/models/')
 
 
@@ -28,13 +28,18 @@ def loadModelFromSDF(modelName, modelNamePrefix = ''):
     linkName = linkTag.attrib['name']
     if isBaseLinkName(modelName, linkName):
       linkName = modelName
-    for meshTag in linkTag.iter('mesh'):
-      for uriTag in meshTag.findall('uri'):
-        meshPath = uriTag.text.replace('model://', 'file://' + modelsPath)
-        tfName = modelNamePrefix + linkName
-        modelPart = (tfName, meshPath)
-        print('Found: %s' % str(modelPart))
-        model.append(modelPart)
+    for visualTag in linkTag.iter('visual'):
+      for poseTag in visualTag.iter('pose'):
+        meshPoseElements = poseTag.text.split()
+        meshPose = Pose()
+        # TODO fill in meshPose from meshPoseElements
+      for meshTag in visualTag.iter('mesh'):
+        for uriTag in meshTag.findall('uri'):
+          meshPath = uriTag.text.replace('model://', 'file://' + modelsPath)
+          tfName = modelNamePrefix + linkName
+          modelPart = (tfName, meshPose, meshPath)
+          print('Found: %s' % str(modelPart))
+          model.append(modelPart)
   
   # TODO recursion for include tag
 
@@ -54,16 +59,6 @@ def on_model_states_msg(modelStatesMsg):
   protoMarkerMsg.id = 0
   protoMarkerMsg.type = Marker.MESH_RESOURCE
   protoMarkerMsg.action = Marker.ADD
-  protoMarkerMsg.pose.position.x = 0.0
-  protoMarkerMsg.pose.position.y = 0.0
-  protoMarkerMsg.pose.position.z = 0.0
-  protoMarkerMsg.pose.orientation.x = 0.0
-  protoMarkerMsg.pose.orientation.y = 0.0
-  protoMarkerMsg.pose.orientation.z = 0.0
-  protoMarkerMsg.pose.orientation.w = 1.0
-  protoMarkerMsg.scale.x = 1.0
-  protoMarkerMsg.scale.y = 1.0
-  protoMarkerMsg.scale.z = 1.0
   protoMarkerMsg.mesh_use_embedded_materials = True
   protoMarkerMsg.color.a = 0.0
   protoMarkerMsg.color.r = 0.0
@@ -76,11 +71,22 @@ def on_model_states_msg(modelStatesMsg):
       modelDict[name] = loadModelFromSDF(name)
 
     for modelPart in modelDict[name]:
-      tfName, meshPath = modelPart
+      tfName, meshPose, meshPath = modelPart
       markerMsg = protoMarkerMsg
       markerMsg.header.frame_id = tfName
       markerMsg.ns = tfName
       markerMsg.mesh_resource = meshPath
+      # TODO meshPose
+      markerMsg.pose.position.x = 0.0
+      markerMsg.pose.position.y = 0.0
+      markerMsg.pose.position.z = 0.0
+      markerMsg.pose.orientation.x = 0.0
+      markerMsg.pose.orientation.y = 0.0
+      markerMsg.pose.orientation.z = 0.0
+      markerMsg.pose.orientation.w = 1.0
+      markerMsg.scale.x = 1.0
+      markerMsg.scale.y = 1.0
+      markerMsg.scale.z = 1.0
       print('Publishing:\n' + str(markerMsg))
       markerPub.publish(markerMsg)
 
