315770592f705f925d69e36dea510ba7e85bcf1b
ahb <andreas.bihlmaier@gmx.de>
2016-03-01 22:43:59 +0100

    uncommit uncommenting of debug output

--- scripts/sdf2moveit_collision.py
+++ scripts/sdf2moveit_collision.py
@@ -121,7 +121,7 @@ def link_to_collision_object(link, full_linkname):
     collision_object.primitives.append(cylinder)
     collision_object.primitive_poses.append(link_pose_in_root_frame)
 
-  print('CollisionObject for %s:\n%s' % (full_linkname, collision_object))
+  #print('CollisionObject for %s:\n%s' % (full_linkname, collision_object))
   return collision_object
 
 
